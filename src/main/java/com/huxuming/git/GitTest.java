c5584824f45d2d3503546d2ca08e79d23521a45f
huxuming0 <100196485+huxuming0@users.noreply.github.com>
2022-07-09 16:14:19 +0800

    huxuming commit

--- src/main/java/com/huxuming/git/GitTest.java
+++ src/main/java/com/huxuming/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello master");
         System.out.println("hello hot-fix");
         System.out.println("hello github");
+        System.out.println("hello huxuming")
     }
 }
