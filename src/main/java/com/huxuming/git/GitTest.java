f03bb7bb063a9ec5e92146acd1b81fcd835fc786
huxuming <huxuming@qq.com>
2022-07-09 13:58:38 +0800

    hot-fix commit

--- src/main/java/com/huxuming/git/GitTest.java
+++ src/main/java/com/huxuming/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello world1");
         System.out.println("hello world2");
         System.out.println("hello world3");
+        System.out.println("hello hot-fix");
     }
 }
