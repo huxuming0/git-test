16c873e396f0ec8feb6a05d56a427369ddac2b76
huxuming <huxuming@qq.com>
2022-07-09 16:57:56 +0800

    second commit

--- src/main/java/com/huxuming/git/GitTest.java
+++ src/main/java/com/huxuming/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello hot-fix");
         System.out.println("hello github");
         System.out.println("hello huxuming");
+        System.out.println("hello UU减速器");
     }
 }
